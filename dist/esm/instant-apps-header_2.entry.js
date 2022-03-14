238d9ac245a43ff19ea592dba5917f97aa6d6cf3
ryan0000 <rlibed@esri.com>
2022-03-14 16:47:21 -0700

    Social share text area text

--- dist/esm/instant-apps-header_2.entry.js
+++
@@ -1,2 +0,0 @@
-export { a3 as instant_apps_header, a4 as instant_apps_social_share } from './instant-apps-header.instant-apps-social-share-bacd4911.js';
-import './index-c2b6dfe1.js';
---
+++ dist/instant-apps-components/instant-apps-social-share.entry.js
@@ -0,0 +1,2 @@
+export { a7 as instant_apps_social_share } from './instant-apps-social-share-d4976aaf.js';
+import './index-4c8f6592.js';
