c365c453bf19c54ed8964b4ca575a3fde9ecbc25
ryan0000 <rlibed@esri.com>
2022-06-13 09:53:10 -0700

    Popovers: t9n updates

---
+++ dist/esm/instant-apps-header.entry.js
@@ -0,0 +1,18 @@
+import { r as registerInstance, h, H as Host } from './index-91b43dd0.js';
+
+const instantAppsHeaderCss = ":host{width:100%}:host header{box-sizing:border-box;display:flex;align-items:center;justify-content:space-between;width:100%;height:5%;padding:0 0.5%;background-color:#0079c1;padding-top:5px;padding-bottom:5px}:host header .instant-apps-header__header-content{display:flex;align-items:center}:host header .instant-apps-header__header-content img{width:6%;padding:0 5px}:host header .instant-apps-header__header-content a{display:flex;align-items:center;width:8%;padding-right:5px}:host header .instant-apps-header__header-content a img{width:100%;padding-right:0}:host header .instant-apps-header__header-content h1{margin:0;font-size:18px;color:var(--calcite-ui-text-inverse);font-weight:430}";
+
+const CSS = {
+  headerContent: 'instant-apps-header__header-content',
+};
+let InstantAppsHeader = class {
+  constructor(hostRef) {
+    registerInstance(this, hostRef);
+  }
+  render() {
+    return (h(Host, null, h("header", { style: { backgroundColor: this.backgroundColor } }, h("span", { class: CSS.headerContent }, this.logoImage && this.logoLink ? (h("a", { href: `${this.logoLink}`, target: "_blank" }, h("img", { src: `${this.logoImage}`, alt: `${this.logoImageAltText}` }))) : this.logoImage ? (h("img", { src: `${this.logoImage}`, alt: this.logoImageAltText })) : (''), h("h1", { style: { color: this.textColor } }, this.titleText)), h("slot", { name: "actions-end" }))));
+  }
+};
+InstantAppsHeader.style = instantAppsHeaderCss;
+
+export { InstantAppsHeader as instant_apps_header };
--- dist/esm/instant-apps-social-share.entry.js
+++ dist/esm/instant-apps-social-share.entry.js
@@ -1,21 +1,6 @@
 import { r as registerInstance, h, H as Host, g as getElement } from './index-91b43dd0.js';
 import { e as esriLoader, g as getLocaleComponentStrings } from './locale-2d6b1670.js';
 
-const instantAppsHeaderCss = ":host{width:100%}:host header{box-sizing:border-box;display:flex;align-items:center;justify-content:space-between;width:100%;height:5%;padding:0 0.5%;background-color:#0079c1;padding-top:5px;padding-bottom:5px}:host header .instant-apps-header__header-content{display:flex;align-items:center}:host header .instant-apps-header__header-content img{width:6%;padding:0 5px}:host header .instant-apps-header__header-content a{display:flex;align-items:center;width:8%;padding-right:5px}:host header .instant-apps-header__header-content a img{width:100%;padding-right:0}:host header .instant-apps-header__header-content h1{margin:0;font-size:18px;color:var(--calcite-ui-text-inverse);font-weight:430}";
-
-const CSS$1 = {
-  headerContent: 'instant-apps-header__header-content',
-};
-let InstantAppsHeader = class {
-  constructor(hostRef) {
-    registerInstance(this, hostRef);
-  }
-  render() {
-    return (h(Host, null, h("header", { style: { backgroundColor: this.backgroundColor } }, h("span", { class: CSS$1.headerContent }, this.logoImage && this.logoLink ? (h("a", { href: `${this.logoLink}`, target: "_blank" }, h("img", { src: `${this.logoImage}`, alt: `${this.logoImageAltText}` }))) : this.logoImage ? (h("img", { src: `${this.logoImage}`, alt: this.logoImageAltText })) : (''), h("h1", { style: { color: this.textColor } }, this.titleText)), h("slot", { name: "actions-end" }))));
-  }
-};
-InstantAppsHeader.style = instantAppsHeaderCss;
-
 /*
  *   Copyright (c) 2022 Esri
  *   All rights reserved.
@@ -469,4 +454,4 @@ let InstantAppsSocialShare = class {
 };
 InstantAppsSocialShare.style = instantAppsSocialShareCss;
 
-export { InstantAppsHeader as instant_apps_header, InstantAppsSocialShare as instant_apps_social_share };
+export { InstantAppsSocialShare as instant_apps_social_share };
