09ca3df37997708278dadb0f632d3605de690a2a
ryan0000 <rlibed@esri.com>
2022-06-19 11:07:46 -0700

    remove generated files

--- dist/types/utils/locale.d.ts
+++
@@ -1,6 +0,0 @@
-declare type StringValue = string | StringBundle;
-interface StringBundle {
-  [key: string]: StringValue;
-}
-export declare function getLocaleComponentStrings<T extends StringBundle = StringBundle>(element: HTMLElement): Promise<[T, string]>;
-export {};
--- dist/types/utils/types.d.ts
+++
@@ -1,1 +0,0 @@
-export declare type APIVersion = 3 | 4;
