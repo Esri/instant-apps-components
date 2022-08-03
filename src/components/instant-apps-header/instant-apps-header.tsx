b29027514740687c43d29612eb2e2cb152ecc780
ryan0000 <rlibed@esri.com>
2022-08-03 12:29:21 -0700

    Header doc update and auto generate license to component README.md files

--- src/components/instant-apps-header/instant-apps-header.tsx
+++ src/components/instant-apps-header/instant-apps-header.tsx
@@ -1,4 +1,5 @@
-/*
+/**
+ *   @license
  *   Copyright (c) 2022 Esri
  *   All rights reserved.
  *   Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,25 +19,44 @@ const CSS = {
   headerContent: 'instant-apps-header__header-content',
 };
 
+/**
+ * @slot actions-end - Used to slot an action at the end of the header i.e. `calcite-action`, `instant-apps-social-share`
+ */
 @Component({
   tag: 'instant-apps-header',
   styleUrl: 'instant-apps-header.scss',
   shadow: true,
 })
 export class InstantAppsHeader {
+  /**
+   * Main text to display in header.
+   */
   @Prop() titleText: string;
 
+  /**
+   * Background color to display in header - accepts a hexidecimal value i.e. `#000000`.
+   */
   @Prop() backgroundColor: string;
 
+  /**
+   * Text color to display in header - accepts a hexidecimal value i.e. `#FFFFFF`.
+   */
   @Prop() textColor: string;
 
+  /**
+   * Image URL for logo. Displays at the start of the header.
+   */
   @Prop() logoImage: string;
 
+  /**
+   * Alternate text for header logo.
+   */
   @Prop() logoImageAltText: string;
 
+  /**
+   * Logo URL to link out to another page.
+   */
   @Prop() logoLink: string;
-
-  @Prop() label: string;
 
   render() {
     return (
