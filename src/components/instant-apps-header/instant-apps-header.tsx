0ea2563870beaf58a940fccf2426a8200aaeca33
Brandon Cree <bcree@esri.com>
2023-04-28 12:18:42 -0700

    fix: update info button

--- src/components/instant-apps-header/instant-apps-header.tsx
+++ src/components/instant-apps-header/instant-apps-header.tsx
@@ -189,7 +189,18 @@ export class InstantAppsHeader {
               {logo}
               {title}
               {this.infoButton ? (
-                <calcite-action icon="information-f" id="infoButton" title={this?.infoTitleText} label={this?.infoTitleText} onClick={this.toggleInfo.bind(this)}></calcite-action>
+                <calcite-button
+                  style={{ '--calcite-ui-text-1': this.textColor }}
+                  id="infoButton"
+                  alignment="start"
+                  appearance="transparent"
+                  kind="neutral"
+                  icon-start="information-f"
+                  scale="s"
+                  label={this.infoTitleText}
+                  title={this.infoTitleText}
+                  onClick={this.toggleInfo.bind(this)}
+                ></calcite-button>
               ) : null}
             </span>
             <slot name="actions-end" />
