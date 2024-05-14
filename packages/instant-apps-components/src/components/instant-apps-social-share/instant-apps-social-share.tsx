b9b5d13758df35200bc387a72d688f4fb9efc456
Ryan Libed <RLibed@esri.com>
2024-05-14 15:39:05 -0700

    fix: remove width and height attribute from X logo svg in social share

--- packages/instant-apps-components/src/components/instant-apps-social-share/instant-apps-social-share.tsx
+++ packages/instant-apps-components/src/components/instant-apps-social-share/instant-apps-social-share.tsx
@@ -523,7 +523,7 @@ export class InstantAppsSocialShare {
   renderXIcon() {
     const isCalciteModeDark = !!this.el.closest(CALCITE_MODE_DARK);
     return (
-      <svg class={isCalciteModeDark ? CSS.xLogo.dark : CSS.xLogo.light} width="1200" height="1227" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg class={isCalciteModeDark ? CSS.xLogo.dark : CSS.xLogo.light} viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect height="400" style={{ fill: 'none' }} width="400" x="56" y="56" />
         <path
           d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
