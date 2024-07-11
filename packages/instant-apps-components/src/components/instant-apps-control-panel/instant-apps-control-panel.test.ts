67e0704c68ed9c272d9856162f18cb30cadf5ff1
JaylenHoLuc <jay13857@esri.com>
2024-07-11 11:15:33 -0700

    dynamic import issue

---
+++ packages/instant-apps-components/src/components/instant-apps-control-panel/instant-apps-control-panel.test.ts
@@ -0,0 +1,26 @@
+import { expect, test } from 'vitest';
+
+import '../../../dist/components/instant-apps-header.js';
+
+const control_panel = document.createElement('instant-apps-control-panel');
+
+test('test if all widgets exist', async () => {
+    // let {entries} = control_panel.components;
+    // for (let i = 0; i < entries.length; i ++){
+    //     entries[i]
+    // }
+    // for (let i = 0 ; i < control_panel.components.length; i++){
+    //     expect(control_panel.components[i].content).toBe(control_panel.components[i]);
+    // }
+    // control_panel.components.slice(-2);
+    // control_panel.componentWillUpdate();
+    // expect(control_panel.components.length).toBe(3);
+    // const arr = control_panel.querySelectorAll('.esri-expand .esri-widget')
+    // expect(arr).toBe(null);
+    // const esri_home = control_panel.querySelectorAll('.esri-home .esri-widget')
+    // expect(esri_home ? true : false).toBe(true);
+    // const esri_zoom = control_panel.querySelectorAll('.esri-zoom .esri-widget')
+    // expect(esri_zoom ? true : false).toBe(true);
+    
+
+});
