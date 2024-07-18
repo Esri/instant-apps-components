0b7df8fd48d09af238a42fd9188eeedc38b8d345
Ryan Libed <RLibed@esri.com>
2024-07-18 13:11:54 -0700

    chore: more updates

--- packages/instant-apps-components/src/components/instant-apps-keyboard-shortcuts/instant-apps-keyboard-shortcuts.stories.ts
+++ packages/instant-apps-components/src/components/instant-apps-keyboard-shortcuts/instant-apps-keyboard-shortcuts.stories.ts
@@ -1,3 +1,11 @@
+/*
+ *   Copyright (c) 2024 Esri
+ *   All rights reserved under the copyright laws of the United States and applicable international laws, treaties, and conventions.
+ *   This material is licensed for use under the Esri Master License Agreement (MLA), and is bound by the terms of that agreement.
+ *   You may redistribute and use this code without modification, provided you adhere to the terms of the MLA and include this copyright notice.
+ *   See use restrictions at http://www.esri.com/legal/pdfs/mla_e204_e300/english
+ */
+
 import SceneView from '@arcgis/core/views/SceneView';
 import WebScene from '@arcgis/core/WebScene';
 import WebMap from '@arcgis/core/WebMap';
