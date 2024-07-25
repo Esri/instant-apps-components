69a009de9355846b41768b905936ad5597601d83
Ryan Libed <RLibed@esri.com>
2024-07-25 12:26:03 -0700

    chore: remove all default test files and set up vitest env

---
+++ packages/instant-apps-components/vitest.config.ts
@@ -0,0 +1,14 @@
+import stencil from 'unplugin-stencil/vite'
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    testTimeout: 10000,
+    browser: {
+      enabled: true,
+      headless: false,
+      name: 'chrome',
+    },
+  },
+  plugins: [stencil()]
+});
