1f30e04210a23e7754d11654b9f97b6652c975ba
Ryan Libed <RLibed@esri.com>
2024-07-30 12:45:57 -0700

    Create unit test example for instant-apps-create

---
+++ packages/instant-apps-components/src/components/instant-apps-create/instant-apps-create.test.ts
@@ -0,0 +1,36 @@
+import { expect, test, describe } from 'vitest';
+import '../../../dist/components/instant-apps-create.js';
+import { waitForElement, waitForShadowRoot } from '../../utils/testUtils.js';
+
+import WebScene from '@arcgis/core/WebScene';
+import config from '@arcgis/core/config';
+
+describe('Instant Apps Create', async () => {
+  config.portalUrl = 'https://nw-brews.mapsdevext.arcgis.com';
+  const scene = new WebScene({
+    portalItem: {
+      id: '0614ea1f9dd043e9ba157b9c20d3c538',
+    },
+  });
+
+  const create = document.createElement('instant-apps-create');
+  create.portal = scene.portalItem.portal;
+  create.content = scene;
+
+  document.body.appendChild(create);
+
+  await new Promise(resolve => requestIdleCallback(resolve));
+  const shadowRoot = await waitForShadowRoot(create);
+
+  test('calcite-popover is present', async () => {
+    create.mode = 'popover';
+    const calcitePopover = await waitForElement('calcite-popover', shadowRoot!);
+    expect(calcitePopover).toBeTruthy();
+  });
+
+  test('calcite-popover is not present', async () => {
+    create.mode = 'inline';
+    const calcitePopover = await waitForElement('calcite-popover', shadowRoot!);
+    expect(calcitePopover).toBeFalsy();
+  });
+});
---
+++ packages/instant-apps-components/src/utils/testUtils.ts
@@ -0,0 +1,34 @@
+export async function waitForShadowRoot(element: HTMLElement): Promise<ShadowRoot | null> {
+  return new Promise<ShadowRoot | null>((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (element.shadowRoot) {
+        clearInterval(interval);
+        resolve(element.shadowRoot);
+      }
+    }, 100); // Check every 100ms
+
+    // Timeout to avoid waiting indefinitely
+    setTimeout(() => {
+      clearInterval(interval);
+      reject(new Error('Timeout: ShadowRoot not found'));
+    }, 5000); // 5 seconds timeout
+  });
+}
+
+export async function waitForElement(selector: string, root: ShadowRoot): Promise<Element | null> {
+  return new Promise<Element | null>((resolve, reject) => {
+    const interval = setInterval(() => {
+      const element = root.querySelector(selector);
+      if (element) {
+        clearInterval(interval);
+        resolve(element);
+      }
+    }, 100); // Check every 100ms
+
+    // Timeout to avoid waiting indefinitely
+    setTimeout(() => {
+      clearInterval(interval);
+      resolve(null); // Resolve with null if element is not found
+    }, 5000); // 5 seconds timeout
+  });
+}
