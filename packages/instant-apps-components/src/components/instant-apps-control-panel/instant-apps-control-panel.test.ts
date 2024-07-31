abdcda1ff29fb7cc48959c216fa12ba71fa94438
JaylenHoLuc <jay13857@esri.com>
2024-07-31 14:13:03 -0700

    fixed some PR requests

--- packages/instant-apps-components/src/components/instant-apps-control-panel/instant-apps-control-panel.test.ts
+++ packages/instant-apps-components/src/components/instant-apps-control-panel/instant-apps-control-panel.test.ts
@@ -7,38 +7,40 @@ import Zoom from '@arcgis/core/widgets/Zoom.js';
 import WebMap from '@arcgis/core/WebMap.js';
 import Legend from '@arcgis/core/widgets/Legend.js';
 const controlPanel = document.createElement('instant-apps-control-panel');
-test('test if all widgets exist in the control panel', async () => {
-  const map = new WebMap({
-    portalItem: {
-      id: '8891ee1d2e0e428bb96c58b8ecf8c408',
-    },
-  });
+describe ("Testing props", () => {
+  test('test if all widgets exist in the control panel', async () => {
+    const map = new WebMap({
+      portalItem: {
+        id: '8891ee1d2e0e428bb96c58b8ecf8c408',
+      },
+    });
 
-  const view = new MapView({
-    container: 'viewDiv',
-    map,
-    ui: {
-      components: [],
-    },
+    const view = new MapView({
+      container: 'viewDiv',
+      map,
+      ui: {
+        components: [],
+      },
+    });
+    const home = new Home({ view });
+    const zoom = new Zoom({ view });
+    const socialShare = document.createElement('instant-apps-social-share');
+    socialShare.classList.add('calcite-mode-light');
+    socialShare.mode = 'inline';
+    const legend = new Legend({
+      view,
+    });
+    const allComponents = [
+      {
+        content: home,
+      },
+      {
+        content: zoom,
+      },
+      { content: legend, isExpand: true, expandTooltip: 'Open legend', collapseTooltip: 'Close legend' },
+      { content: socialShare, isExpand: true, expandIcon: 'share', expandTooltip: 'Open social share', collapseTooltip: 'Close social share' },
+    ];
+    controlPanel.components = allComponents;
+    expect(controlPanel.components).toBe(allComponents);
   });
-  const home = new Home({ view });
-  const zoom = new Zoom({ view });
-  const socialShare = document.createElement('instant-apps-social-share');
-  socialShare.classList.add('calcite-mode-light');
-  socialShare.mode = 'inline';
-  const legend = new Legend({
-    view,
-  });
-  const all_components = [
-    {
-      content: home,
-    },
-    {
-      content: zoom,
-    },
-    { content: legend, isExpand: true, expandTooltip: 'Open legend', collapseTooltip: 'Close legend' },
-    { content: socialShare, isExpand: true, expandIcon: 'share', expandTooltip: 'Open social share', collapseTooltip: 'Close social share' },
-  ];
-  controlPanel.components = all_components;
-  expect(controlPanel.components).toBe(all_components);
 });
--- packages/instant-apps-components/src/components/instant-apps-header/instant-apps-header.test.ts
+++ packages/instant-apps-components/src/components/instant-apps-header/instant-apps-header.test.ts
@@ -1,66 +1,75 @@
-import { expect, test } from 'vitest';
+import { expect, test, describe } from 'vitest';
 import '../../../dist/components/instant-apps-header.js';
 const header = document.createElement('instant-apps-header');
 header.setAttribute('id', 'instant-apps-header');
 header.setAttribute('background-color', '#151515');
-test('should render component with custom title text', async () => {
-  const customTitle = 'Instant Apps Header';
-  header.titleText = customTitle;
-  document.body.appendChild(header);
-  await new Promise(resolve => requestIdleCallback(resolve));
 
-  expect(header.titleText).toBe(customTitle);
-});
+describe ("Testing prop changes", () => {
+  test('should render component with custom title text', async () => {
+    const customTitle = 'Instant Apps Header';
+    header.titleText = customTitle;
+    document.body.appendChild(header);
+    await new Promise(resolve => requestIdleCallback(resolve));
 
-test('test info button visibility', async () => {
-  header.infoButton = true;
-  header.setAttribute('info-button', 'true');
-  header.infoTitleText = 'info test';
-  document.body.appendChild(header);
-  await new Promise(resolve => requestIdleCallback(resolve));
-  header.toggleInfo();
-  expect(header.infoButton).toBe(true);
-  expect(header.infoIsOpen).toBe(true);
-  header.toggleInfo();
-  expect(header.infoIsOpen).toBe(false);
-  const infoElement = header.shadowRoot?.getElementById('infoButton');
-  expect(infoElement ? true : false).toBe(true);
-  expect(infoElement).toBeTruthy();
-  const infosTitle = infoElement?.getAttribute('title');
-  expect(infosTitle).toBe(header.infoTitleText);
-});
+    expect(header.titleText).toBe(customTitle);
+  });
 
-test('logo scale change should be reflected', async () => {
-  header.logoScale = 's';
-  header.setAttribute('logo-scale', 's');
-  document.body.appendChild(header);
-  await new Promise(resolve => requestIdleCallback(resolve));
-  const elem = document.getElementById('instant-apps-header');
-  expect(elem?.getAttribute('logo-scale')).toBe('s');
-});
+  test('test info button visibility', async () => {
+    header.infoButton = true;
+    header.setAttribute('info-button', 'true');
+    header.infoTitleText = 'info test';
+    document.body.appendChild(header);
+    await new Promise(resolve => requestIdleCallback(resolve));
+    header.toggleInfo();
+    expect(header.infoButton).toBe(true);
+    expect(header.infoIsOpen).toBe(true);
+  });
 
-test('logo scale change to large should be reflected', async () => {
-  header.logoScale = 'l';
-  header.setAttribute('logo-scale', 'l');
-  document.body.appendChild(header);
-  await new Promise(resolve => requestIdleCallback(resolve));
-  const elem = document.getElementById('instant-apps-header');
-  expect(elem?.getAttribute('logo-scale')).toBe('l');
-});
+  test('test info button toggle', async () => {
+    header.toggleInfo();
+    expect(header.infoIsOpen).toBe(false);
+  });
 
-test('change background color', async () => {
-  const newColor = '#836953';
-  header.setAttribute('background-color', newColor);
-  document.body.appendChild(header);
-  await new Promise(resolve => requestIdleCallback(resolve));
-  const elem = document.getElementById('instant-apps-header');
-  expect(elem?.getAttribute('background-color')).toBe(newColor);
-});
+  test('test info text title prop', async () => {
+    const infoElement = header.shadowRoot?.getElementById('infoButton');
+    expect(infoElement ? true : false).toBe(true);
+    expect(infoElement).toBeTruthy();
+    const infosTitle = infoElement?.getAttribute('title');
+    expect(infosTitle).toBe(header.infoTitleText);
+  })
 
-test('set title text link', async () => {
-  const newLink = 'https://doc.arcgis.com/en/instant-apps/gallery/';
-  header.titleTextLink = newLink;
-  document.body.appendChild(header);
-  await new Promise(resolve => requestIdleCallback(resolve));
-  expect(header.titleTextLink).toBe(newLink);
-});
+  test('logo scale change should be reflected', async () => {
+    header.logoScale = 's';
+    header.setAttribute('logo-scale', 's');
+    document.body.appendChild(header);
+    await new Promise(resolve => requestIdleCallback(resolve));
+    const elem = document.getElementById('instant-apps-header');
+    expect(elem?.getAttribute('logo-scale')).toBe('s');
+  });
+
+  test('logo scale change to large should be reflected', async () => {
+    header.logoScale = 'l';
+    header.setAttribute('logo-scale', 'l');
+    document.body.appendChild(header);
+    await new Promise(resolve => requestIdleCallback(resolve));
+    const elem = document.getElementById('instant-apps-header');
+    expect(elem?.getAttribute('logo-scale')).toBe('l');
+  });
+
+  test('change background color', async () => {
+    const newColor = '#836953';
+    header.setAttribute('background-color', newColor);
+    document.body.appendChild(header);
+    await new Promise(resolve => requestIdleCallback(resolve));
+    const elem = document.getElementById('instant-apps-header');
+    expect(elem?.getAttribute('background-color')).toBe(newColor);
+  });
+
+  test('set title text link', async () => {
+    const newLink = 'https://doc.arcgis.com/en/instant-apps/gallery/';
+    header.titleTextLink = newLink;
+    document.body.appendChild(header);
+    await new Promise(resolve => requestIdleCallback(resolve));
+    expect(header.titleTextLink).toBe(newLink);
+  });
+})
