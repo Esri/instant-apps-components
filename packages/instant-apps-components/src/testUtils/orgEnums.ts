689bc210e2be26b541b1a69b3ee55eda68793e72
Ryan Libed <RLibed@esri.com>
2024-08-05 14:26:49 -0700

    fix: org name

--- packages/instant-apps-components/src/testUtils/orgEnums.ts
+++ packages/instant-apps-components/src/testUtils/orgEnums.ts
@@ -1,6 +1,6 @@
 enum EArcGISOrgs {
   Holistic = 'https://holistic.mapsdevext.arcgis.com',
-  NWBrews = 'https://nwbrews.mapsdevext.arcgis.com',
+  NWBrews = 'https://nw-brews.mapsdevext.arcgis.com',
 }
 
 export default EArcGISOrgs;
