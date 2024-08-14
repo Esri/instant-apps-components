64d3b117bf522b08431f363f42f4419960c57c1b
JaylenHoLuc <jay13857@esri.com>
2024-08-14 16:17:58 -0700

    robust checking of props and refactoring 2

--- packages/instant-apps-components/src/components/instant-apps-header/instant-apps-header.test.ts
+++ packages/instant-apps-components/src/components/instant-apps-header/instant-apps-header.test.ts
@@ -4,9 +4,9 @@ import { waitForShadowRoot } from '../../utils/testUtils.js';
 describe('Header', () => {
   const header = document.createElement('instant-apps-header');
   header.setAttribute('background-color', '#151515');
-  header.setAttribute('logo-image', "https://www.esri.com/content/dam/esrisites/en-us/common/icons/product-logos/arcgis-instant-apps-64.svg");
-  header.setAttribute('logo-link', "https://www.esri.com/en-us/arcgis/products/arcgis-instant-apps/overview");
-  header.titleTextLink = 'https://doc.arcgis.com/en/instant-apps/gallery/'; 
+  header.setAttribute('logo-image', 'https://www.esri.com/content/dam/esrisites/en-us/common/icons/product-logos/arcgis-instant-apps-64.svg');
+  header.setAttribute('logo-link', 'https://www.esri.com/en-us/arcgis/products/arcgis-instant-apps/overview');
+  header.titleTextLink = 'https://doc.arcgis.com/en/instant-apps/gallery/';
   let shadow;
   let element;
   beforeEach(async () => {
@@ -14,8 +14,7 @@ describe('Header', () => {
     await new Promise(resolve => requestIdleCallback(resolve));
     shadow = await waitForShadowRoot(document.querySelector('instant-apps-header')!);
     element = document.querySelector('instant-apps-header');
-
-  }); 
+  });
   describe('titleText prop', async () => {
     test('custom title text', async () => {
       const customTitle = 'Instant Apps Header';
@@ -28,13 +27,12 @@ describe('Header', () => {
     });
     test('custom title text in render title h1', async () => {
       expect(element).toBeTruthy();
-     
-      const h1 = shadow?.querySelector("h1");
+
+      const h1 = shadow?.querySelector('h1');
       expect(h1!.innerHTML).toBe(header.titleText);
     });
   });
   describe('infoTitleText prop', async () => {
-
     test('info button visibility', async () => {
       header.setAttribute('info-button', 'true');
       header.infoTitleText = 'info test';
@@ -43,44 +41,40 @@ describe('Header', () => {
       header.toggleInfo();
       expect(element).toBeTruthy();
 
-      console.log("toggle -> ", element!.infoIsOpen);
       expect(element!.infoButton).toBe(true);
       expect(element!.infoIsOpen).toBe(true);
     });
     test('info button toggle', async () => {
       expect(element).toBeTruthy();
-      console.log("info open -> ", element?.getAttribute("info-is-open"));
-      expect(element?.getAttribute("info-is-open")).toBe("");
+      console.log('info open -> ', element?.getAttribute('info-is-open'));
+      expect(element?.getAttribute('info-is-open')).toBe('');
       header.toggleInfo();
       expect(element!.infoIsOpen).toBe(false);
     });
-    test("infoTitleText prop in calcite button", async () => {
-      const button = shadow?.querySelector("calcite-button");
+    test('infoTitleText prop in calcite button', async () => {
+      const button = shadow?.querySelector('calcite-button');
       expect(button?.title).toBe(header.infoTitleText);
-      
-    })
+    });
   });
   describe('logoimage prop', async () => {
-    test("logoImage on header", async () => {
-      expect(element?.getAttribute("logo-image")).toBe(header.logoImage);
-      
-    })
-    test("logoImage on img tag", async () => {
-      const imgTag = shadow?.querySelector("img");
-      expect(imgTag?.getAttribute("src")).toBe(header.logoImage);
-    })
+    test('logoImage on header', async () => {
+      expect(element?.getAttribute('logo-image')).toBe(header.logoImage);
+    });
+    test('logoImage on img tag', async () => {
+      const imgTag = shadow?.querySelector('img');
+      expect(imgTag?.getAttribute('src')).toBe(header.logoImage);
+    });
   });
   describe('logoLink prop', async () => {
-    test("logoLink on header", async () => {
-      expect(element?.getAttribute("logo-link")).toBe(header.logoLink);
-      
-    })
-    test("logoLink on a tag", async () => {
+    test('logoLink on header', async () => {
+      expect(element?.getAttribute('logo-link')).toBe(header.logoLink);
+    });
+    test('logoLink on a tag', async () => {
       const aTag = shadow?.querySelector(`a[href='${header.logoLink}']`);
       expect(aTag).toBeTruthy();
-    })
+    });
   });
-  describe("logo scale prop", async () => {
+  describe('logo scale prop', async () => {
     test('logo scale', async () => {
       header.logoScale = 's';
       header.setAttribute('logo-scale', 's');
@@ -89,8 +83,6 @@ describe('Header', () => {
       expect(element).toBeTruthy();
       expect(element!.logoScale).toBe(header.logoScale);
     });
-
-
 
     test('logo scale', async () => {
       header.logoScale = 'l';
@@ -104,11 +96,10 @@ describe('Header', () => {
       document.body.appendChild(header);
       await new Promise(resolve => requestIdleCallback(resolve));
       expect(shadow).toBeTruthy();
-      const logoElem = shadow.querySelector("img");
+      const logoElem = shadow.querySelector('img');
       expect(logoElem).toBeTruthy();
       expect(logoElem?.classList.contains(`instant-apps-header__logo-scale--${header.logoScale}`)).toBe(true);
-    })
-
+    });
   });
   test('change background color', async () => {
     const newColor = '#836953';
@@ -118,9 +109,8 @@ describe('Header', () => {
     expect(element).toBeTruthy();
     expect(element!.backgroundColor).toBe(header.backgroundColor);
   });
-  describe("titleTextLink prop", async () => {
+  describe('titleTextLink prop', async () => {
     test('set title text link', async () => {
-      
       expect(element).toBeTruthy();
       const titleElem = shadow.querySelector(`a[href='${header.titleTextLink}']`);
       expect(titleElem).toBeTruthy();
