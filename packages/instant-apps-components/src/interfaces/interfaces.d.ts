e7362ff494bbc912811f7d14a2d9f7d46f366061
Ryan Libed <RLibed@esri.com>
2024-08-22 12:03:58 -0700

    feat: add support for expanded property in control panel

--- packages/instant-apps-components/src/interfaces/interfaces.d.ts
+++ packages/instant-apps-components/src/interfaces/interfaces.d.ts
@@ -273,6 +273,7 @@ export interface ControlPanelComponent {
   expandIcon?: string;
   expandTooltip?: string;
   collapseTooltip?: string;
+  expanded?: boolean;
 }
 
 export interface IPortal extends __esri.Portal {
